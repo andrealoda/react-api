3af2c1d725c6bde49af8488b7ad768e5aae4dceb
andrea8980 <andrea@casaloda.it>
2026-05-08 10:48:53 +0200

    Milestone 1 - log APIs in console

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-    </>
-  )
+  const api_actors = 'https://lanciweb.github.io/demo/api/actors/'
+  const api_actress = 'https://lanciweb.github.io/demo/api/actresses/'
+
+  useEffect(() => {
+    console.log('component mounted');
+
+    fetch(api_actors)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+
+    fetch(api_actress)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+
+  }, [])
+
+
+
+return (
+  <>
+
+
+
+  </>
+)
 }
 
 export default App
